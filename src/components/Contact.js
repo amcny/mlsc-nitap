a8b5808ed173ad1ea5989bc7008837b94902dad5
amcny <cny2045@gmail.com>
2024-04-12 09:27:08 +0530

    updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,11 +7,11 @@ import { faGithub, faWhatsapp, faDiscord, faMicrosoft } from '@fortawesome/free-
 const Contact = () => {
     return (
         <div className='contact'>
-            <div className='contact-info'><h4>Microsoft Ambassadors</h4>
+            <div className='contact-info'><h4>Microsoft Ambassador</h4>
                 <p>Chaitanya Pullagura</p></div>
             <div className='contact-info'><h4>Phone</h4>
                 <a href='tel:+919440368831'>+919440368831</a></div>
-            <div className='contact-info'><h4>Emails</h4>
+            <div className='contact-info'><h4>Email</h4>
                 <a href='mailto:Chaitanya.Pullagura@studentambassadors.com'>Chaitanya.Pullagura@studentambassadors.com</a></div>
             <div className='contact-info'><h4>LinkedIn</h4>
                 <a target='_blank' href="https://www.linkedin.com/in/chaitanyapullagura/" >Chaitanya Pullagura</a></div>
